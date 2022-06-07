d8338b494a07f154199c1a5478bd55a60e8f0444
Daniel Reha <rehadaniel.personal@gmail.com>
2022-06-07 18:04:34 +0200

    Updated comments

--- src/Core/EmailProcessingApp.Application/Features/Commands/ProcessEmailDataCommand/CreateResponseEmailCommand/CreateResponseEmailCommandHandler.cs
+++ src/Core/EmailProcessingApp.Application/Features/Commands/ProcessEmailDataCommand/CreateResponseEmailCommand/CreateResponseEmailCommandHandler.cs
@@ -28,20 +28,20 @@ namespace EmailProcessingApp.Application.Features.Commands.ProcessEmailDataComma
             // Fetching response email for the current date
             var email = await _repository.FindByEmailAndDateAsync(request.Email, DateTime.Now);
 
-            // If we already created one previously we return and don't do anything
+            // If we have already created one previously we close the event without action
             if (email != null)
             {
                 return response;
             }
 
-            // We get the custom message template from the appropriate service and the format it correctly
+            // We fetch the custom message template from the appropriate service and then format it correctly
             var messageTemplate = await _messageTemplateService.GetMessageTemplateAsync(MessageTemplateType.ResponseEmailBody);
             var messageBody = messageTemplate.Replace("{}", string.Join(",", request.Attributes));
 
             // Inserting the response email in the sendemails table
             await _repository.AddAsync(new ResponseEmail() { Email = request.Email, EmailBody = Encoding.UTF8.GetBytes(messageBody), IsSent = false });
 
-            // Storing the response email to blob storage also in the appropriate container
+            // Storing the response email to blob storage in the appropriate container
             var blobName = $"{DateTime.Now.ToShortDateString().Replace("/", "-")}_{request.Email}.txt";
             await _blobService.AppendToBlobAsync(blobName, messageBody, BlobContainerType.EmailBodyContainer);
 
